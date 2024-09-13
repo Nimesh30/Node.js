830c7ad817ef63fc186423e7b03761026252190d
Nimesh30 <nimprajapati2004@gmail.com>
2024-09-14 00:17:23 +0530

    server creation with expressJS

---
+++ node_4.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.send(
+    "<h1>Hello from Home page  </h1>"
+  );
+});
+
+app.get("/about", (req, res) => {
+  return res.send("<h1>Hello from About Page..</h1>");
+});
+
+app.get("/search", (req, res) => {
+  return res.send("<h1>Hello from About Page..</h1>");
+});
+
+app.listen(8000, () => console.log("Server Started..."));
+// const myserver = http.createServer(app);
+// myserver.listen(8000,()=> console.log("Server Started..."))
